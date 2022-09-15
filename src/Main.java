d6acb211327d8fe0d06680b091676dfd3f20727a
mahmoodalgbory <algbormd@mail.uc.edu>
2022-09-15 18:40:08 -0400

    task1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args)
+    {
+    double purchasePrice = 25;
+    double salesTax = purchasePrice * 1.05;
+    System.out.println("If the purchase is " + purchasePrice + " the total price with a 5% sales tax is " + salesTax);
     }
 }
